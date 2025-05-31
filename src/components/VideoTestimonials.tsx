8502a3fe0d497392246900a4a93e6c3108628247
tiago-forward <devtiagoforward@gmail.com>
2025-05-31 11:22:50 -0300

    some adjustments

---
+++ src/components/VideoTestimonials.tsx
@@ -0,0 +1,26 @@
+interface VideoTestimonialsProps {
+  videos: { src: string; title: string }[];
+}
+
+export default function VideoTestimonials({ videos }: VideoTestimonialsProps) {
+  return (
+    <div className="pb-10 px-4 sm:px-8 lg:px-16">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 max-w-5xl mx-auto">
+        {videos.map((video, index) => (
+          <div
+            key={index}
+            className="w-full h-[600px] rounded-xl overflow-hidden shadow-md border border-border"
+          >
+            <video
+              controls
+              className="w-full h-full object-cover"
+            >
+              <source src={video.src} type="video/mp4" />
+              Seu navegador não suporta vídeos HTML5.
+            </video>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/components/sections/Testimonials.tsx
+++ src/components/sections/Testimonials.tsx
@@ -1,10 +1,12 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Navigation, Autoplay } from "swiper/modules";
 import "swiper/css";
+import "swiper/css/navigation";
 import "swiper/css/pagination";
-import ArrowRight from "../Icons/ArrowRight";
-import ArrowLeft from "../Icons/ArrowLeft";
-import { useRef } from "react";
+// import ArrowRight from "../Icons/ArrowRight";
+// import ArrowLeft from "../Icons/ArrowLeft";
+// import { useRef } from "react";
+import VideoTestimonials from "../VideoTestimonials";
 
 const googleReviews = [
   {
@@ -56,54 +58,77 @@ export default function Testimonials() {
         </p>
       </div>
 
-      <Swiper
-        modules={[Navigation, Pagination, Autoplay]}
-        spaceBetween={24}
-        slidesPerView={1}
-        loop={true}
-        pagination={{ clickable: true }}
-        autoplay={{ delay: 3000 }}
-        breakpoints={{
-          768: { slidesPerView: 1.2 },
-          1024: { slidesPerView: 3 },
-        }}
-        className="max-w-screen-xl"
-      >
-        {googleReviews.map((review, index) => (
-          <SwiperSlide key={index}>
-            <div className="mb-8 bg-white border border-border rounded-[2rem] px-6 py-8 md:px-8 flex flex-col justify-between h-full transition hover:scale-[1.01] duration-300">
-              <div className="flex items-center gap-4 mb-6">
-                <img
-                  src={review.foto}
-                  alt={review.nome}
-                  className="w-14 h-14 rounded-full object-cover border-2 border-primary"
-                />
-                <div className="text-left">
-                  <p className="text-lg font-semibold text-txt-tertiary">
-                    {review.nome}
-                  </p>
-                  <div className="text-yellow-400 text-2xl">
-                    {"★".repeat(review.estrelas)}
-                    {"☆".repeat(5 - review.estrelas)}
+      <VideoTestimonials
+        videos={[
+          {
+            src: "/public/videos/A Transformação do Diego.mp4",
+            title: "Depoimento da Camila",
+          },
+          {
+            src: "/public/videos/Carlos - Depoimento .mp4",
+            title: "Depoimento do Marcos",
+          },
+          {
+            src: "/public/videos/Depoimento .mp4",
+            title: "Depoimento da Ana",
+          },
+          {
+            src: "/public/videos/Depoimento Anderson.mp4",
+            title: "Depoimento do João",
+          },
+        ]}
+      />
+
+      <div>
+        <Swiper
+          modules={[Navigation, Pagination, Autoplay]}
+          spaceBetween={24}
+          slidesPerView={1}
+          loop={true}
+          pagination={{ clickable: true }}
+          autoplay={{ delay: 3000 }}
+          breakpoints={{
+            768: { slidesPerView: 1.2 },
+            1024: { slidesPerView: 3 },
+          }}
+          className="max-w-screen-xl"
+        >
+          {googleReviews.map((review, index) => (
+            <SwiperSlide key={index}>
+              <div className="mb-8 bg-white border border-border rounded-[2rem] px-6 py-8 md:px-8 flex flex-col justify-between h-full transition hover:scale-[1.01] duration-300">
+                <div className="flex items-center gap-4 mb-6">
+                  <img
+                    src={review.foto}
+                    alt={review.nome}
+                    className="w-14 h-14 rounded-full object-cover border-2 border-primary"
+                  />
+                  <div className="text-left">
+                    <p className="text-lg font-semibold text-txt-tertiary">
+                      {review.nome}
+                    </p>
+                    <div className="text-yellow-400 text-2xl">
+                      {"★".repeat(review.estrelas)}
+                      {"☆".repeat(5 - review.estrelas)}
+                    </div>
                   </div>
                 </div>
+
+                <p className="text-md text-txt-tertiary leading-relaxed">
+                  “
+                  {review.comentario.length > 180
+                    ? `${review.comentario.slice(0, 180)}...`
+                    : review.comentario}
+                  ”
+                </p>
+
+                <span className="mt-6 text-sm text-txt-tertiary opacity-70">
+                  Avaliação verificada no Google
+                </span>
               </div>
-
-              <p className="text-md text-txt-tertiary leading-relaxed">
-                “
-                {review.comentario.length > 180
-                  ? `${review.comentario.slice(0, 180)}...`
-                  : review.comentario}
-                ”
-              </p>
-
-              <span className="mt-6 text-sm text-txt-tertiary opacity-70">
-                Avaliação verificada no Google
-              </span>
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </div>
   );
 }
